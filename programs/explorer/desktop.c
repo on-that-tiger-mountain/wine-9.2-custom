d8110e697f5bc983dd02d36664b111112a98fe2b
on-that-tiger-mountain <15920418725@139.com>
2026-05-15 00:32:47 +0800

    update programs/explorer/desktop.c

--- programs/explorer/desktop.c
+++ programs/explorer/desktop.c
@@ -1112,7 +1112,6 @@ void manage_desktop( WCHAR *arg )
         }
         SetThreadDesktop( desktop );
     }
-    else ExitProcess( 0 );
 
     /* create the desktop window */
     hwnd = CreateWindowExW( 0, DESKTOP_CLASS_ATOM, NULL,
